20917fd41cc289493934a17416c9efe3d36468b3
张启航 <zqh0104@163.com>
2020-11-23 18:44:32 +0800

    zhifu

--- foodie-dev-api/src/main/java/com/imooc/controller/OrdersController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/OrdersController.java
@@ -1,17 +1,25 @@
 package com.imooc.controller;
 
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.api.ApiController;
-import com.baomidou.mybatisplus.extension.api.R;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.imooc.pojo.Orders;
+import com.imooc.commom.utils.IMOOCJSONResult;
+import com.imooc.pojo.bo.SubmitOrderBO;
+import com.imooc.pojo.vo.MerchantOrdersVO;
+import com.imooc.pojo.vo.OrderVO;
 import com.imooc.service.OrdersService;
-import org.springframework.web.bind.annotation.*;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
-import java.io.Serializable;
-import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 订单表;(Orders)表控制层
@@ -19,68 +27,70 @@ import java.util.List;
  * @author 张启航
  * @since 2020-11-13 18:07:05
  */
+@Api(value = "订单相关", tags = {"订单相关的api接口"})
 @RestController
 @RequestMapping("orders")
 public class OrdersController extends ApiController {
     /**
      * 服务对象
      */
-    @Resource
+    @Autowired
     private OrdersService ordersService;
 
-    /**
-     * 分页查询所有数据
-     *
-     * @param page   分页对象
-     * @param orders 查询实体
-     * @return 所有数据
-     */
-    @GetMapping
-    public R selectAll(Page<Orders> page, Orders orders) {
-        return success(this.ordersService.page(page, new QueryWrapper<>(orders)));
+    @ApiOperation(value = "用户下单", notes = "用户下单", httpMethod = "POST")
+    @PostMapping("/create")
+    public IMOOCJSONResult create(
+            @RequestBody SubmitOrderBO submitOrderBO,
+            HttpServletRequest request,
+            HttpServletResponse response) {
+
+        if (submitOrderBO.getPayMethod() != PayMethod.WEIXIN.type
+                && submitOrderBO.getPayMethod() != PayMethod.ALIPAY.type ) {
+            return IMOOCJSONResult.errorMsg("支付方式不支持！");
+        }
+
+//        System.out.println(submitOrderBO.toString());
+
+        // 1. 创建订单
+        OrderVO orderVO = orderService.createOrder(submitOrderBO);
+        String orderId = orderVO.getOrderId();
+
+        // 2. 创建订单以后，移除购物车中已结算（已提交）的商品
+        /**
+         * 1001
+         * 2002 -> 用户购买
+         * 3003 -> 用户购买
+         * 4004
+         */
+        // TODO 整合redis之后，完善购物车中的已结算商品清除，并且同步到前端的cookie
+//        CookieUtils.setCookie(request, response, FOODIE_SHOPCART, "", true);
+
+        // 3. 向支付中心发送当前订单，用于保存支付中心的订单数据
+        MerchantOrdersVO merchantOrdersVO = orderVO.getMerchantOrdersVO();
+        merchantOrdersVO.setReturnUrl(payReturnUrl);
+
+        // 为了方便测试购买，所以所有的支付金额都统一改为1分钱
+        merchantOrdersVO.setAmount(1);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.add("imoocUserId","imooc");
+        headers.add("password","imooc");
+
+        HttpEntity<MerchantOrdersVO> entity =
+                new HttpEntity<>(merchantOrdersVO, headers);
+
+        ResponseEntity<IMOOCJSONResult> responseEntity =
+                restTemplate.postForEntity(paymentUrl,
+                        entity,
+                        IMOOCJSONResult.class);
+        IMOOCJSONResult paymentResult = responseEntity.getBody();
+        if (paymentResult.getStatus() != 200) {
+            logger.error("发送错误：{}", paymentResult.getMsg());
+            return IMOOCJSONResult.errorMsg("支付中心订单创建失败，请联系管理员！");
+        }
+
+        return IMOOCJSONResult.ok(orderId);
     }
 
-    /**
-     * 通过主键查询单条数据
-     *
-     * @param id 主键
-     * @return 单条数据
-     */
-    @GetMapping("{id}")
-    public R selectOne(@PathVariable Serializable id) {
-        return success(this.ordersService.getById(id));
-    }
-
-    /**
-     * 新增数据
-     *
-     * @param orders 实体对象
-     * @return 新增结果
-     */
-    @PostMapping
-    public R insert(@RequestBody Orders orders) {
-        return success(this.ordersService.save(orders));
-    }
-
-    /**
-     * 修改数据
-     *
-     * @param orders 实体对象
-     * @return 修改结果
-     */
-    @PutMapping
-    public R update(@RequestBody Orders orders) {
-        return success(this.ordersService.updateById(orders));
-    }
-
-    /**
-     * 删除数据
-     *
-     * @param idList 主键结合
-     * @return 删除结果
-     */
-    @DeleteMapping
-    public R delete(@RequestParam("idList") List<Long> idList) {
-        return success(this.ordersService.removeByIds(idList));
-    }
 }
