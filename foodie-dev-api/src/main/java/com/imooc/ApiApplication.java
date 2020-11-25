cdd6cbdb92cc7a7d78015ca0f1cbd4b579c7611c
张启航 <zqh0104@163.com>
2020-11-25 18:20:49 +0800

    定时任务（关闭未支付订单）

--- foodie-dev-api/src/main/java/com/imooc/ApiApplication.java
+++ foodie-dev-api/src/main/java/com/imooc/ApiApplication.java
@@ -3,6 +3,7 @@ package com.imooc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @Author: QiHangZhang
@@ -13,6 +14,8 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 // 扫描所有包以及相关组件包
 @ComponentScan(basePackages = {"com.imooc","org.n3r.idworker"})
+//开启定时任务
+@EnableScheduling
 public class ApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class,args);
