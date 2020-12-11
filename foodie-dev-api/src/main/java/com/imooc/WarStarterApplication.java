260d6aabd59c364930325a880a224e373fbf7b33
张启航 <zqh0104@163.com>
2020-12-11 18:35:06 +0800

    部署到服务器（静态资源路径无法访问--头像）

---
+++ foodie-dev-api/src/main/java/com/imooc/WarStarterApplication.java
@@ -0,0 +1,15 @@
+package com.imooc;
+
+/**
+ * @Author: 张启航
+ * @Date: 2020/12/11 13:27
+ * @Description: 打包war[4] 增加war的启动类
+ */
+//public class WarStarterApplication extends SpringBootServletInitializer {
+//
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        //指向ApiApplication这个springboot启动类
+//        return builder.sources(ApiApplication.class);
+//    }
+//}
--- foodie-dev-api/src/main/java/com/imooc/controller/BaseController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/BaseController.java
@@ -21,7 +21,7 @@ public class BaseController {
 
     // 微信支付成功 -> 支付中心 -> 天天吃货平台
     //                       |-> 回调通知的url
-    String payReturnUrl = "http://sqdhzi.natappfree.cc/orders/notifyMerchantOrderPaid";
+    String payReturnUrl = "http://yawei.wang:8088/foodie-dev-api/orders/notifyMerchantOrderPaid";
 
     // 用户上传头像的位置
     public static final String IMAGE_USER_FACE_LOCATION = File.separator + "develop" +
